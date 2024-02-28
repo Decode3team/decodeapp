793ad6724ed4da4dd8e0d8c6b48cd9c68ba600b9
palaganas.ja@gmail.com <palaganas.ja@gmail.com>
2024-02-28 15:05:58 +0800

    Linting fixes and added changes to support CI/CD

--- lib/redis/client.ts
+++ lib/redis/client.ts
@@ -1,13 +1,16 @@
-import Redis from 'ioredis';
+import { Redis, RedisOptions } from 'ioredis';
 
 export class RedisClient {
   private client: Redis;
 
   constructor() {
-    this.client = new Redis({
+    const redisOptions: RedisOptions = {
       host: process.env.REDIS_ENDPOINT ?? 'redis',
       port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
-    });
+      password: process.env.REDIS_PASSWORD,
+    };
+
+    this.client = new Redis(redisOptions);
   }
 
   async set(key: string, value: string, expirationInSeconds?: number) {
@@ -22,6 +25,7 @@ export class RedisClient {
 
   async get(key: string) {
     const result = await this.client.get(key);
+
     return result;
   }
 
