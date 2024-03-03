2513c46b294048ed451181c2254a1eea426e24fd
palaganas.ja@gmail.com <palaganas.ja@gmail.com>
2024-03-03 20:52:33 +0800

    Removed unecessary comments

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,22 +1,12 @@
-/**
- * This file contains the tRPC http response handler and context creation for Next.js
- */
 import { AppRouter, appRouter } from '@/server/routers';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { createContext } from 'server/context';
 
 export default trpcNext.createNextApiHandler<AppRouter>({
   router: appRouter,
-  /**
-   * @link https://trpc.io/docs/v11/context
-   */
   createContext,
-  /**
-   * @link https://trpc.io/docs/v11/error-handling
-   */
   onError({ error }) {
     if (error.code === 'INTERNAL_SERVER_ERROR') {
-      // send to bug reporting
       console.error('Something went wrong', error);
     }
   },
