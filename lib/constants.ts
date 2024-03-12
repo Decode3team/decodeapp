6a1e797590a4beb9329667042017ca91543c82cb
palaganas.ja@gmail.com <palaganas.ja@gmail.com>
2024-03-12 19:02:46 +0800

    More fixes

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,19 +1,24 @@
 import { DefinedApiTimeResolution } from './defined/types';
 
-export const apiPort = process.env.PUBLIC_WEBSOCKET_HOST_PORT
-  ? Number(process.env.PUBLIC_WEBSOCKET_HOST_PORT)
-  : 80;
+export const apiPort = process.env.PUBLIC_API_HOST_PORT
+  ? Number(process.env.PUBLIC_API_HOST_PORT)
+  : 3001;
 
 export const hostPort = process.env.NEXT_PUBLIC_HOST_PORT
   ? Number(process.env.NEXT_PUBLIC_HOST_PORT)
-  : 80;
+  : 3000;
 
-export const hostDomain = process.env.NEXT_PUBLIC_HOST_DOMAIN ?? 'decode-dev-as.azurewebsites.net';
-export const apiDomain = process.env.PUBLIC_API_HOST_DOMAIN ?? 'decode-dev-api-as.azurewebsites.net';
+export const httpApiProtocol = process.env.PUBLIC_API_HOST_PORT_PROTOCOL ?? 'http';
+export const wsApiProtocol = httpApiProtocol === 'http' ? 'ws' : 'wss';
+export const hostProtocol = process.env.NEXT_PUBLIC_HOST_PORT_PROTOCOL ?? 'http';
 
-export const wsApiHostUrl = `ws://${apiDomain}:${apiPort}`;
-export const httpApiHostUrl = `http://${apiDomain}:${apiPort}`;
-export const hostUrl = `http://${hostDomain}:${hostPort}`;
+
+export const hostDomain = process.env.NEXT_PUBLIC_HOST_DOMAIN ?? 'localhost';
+export const apiDomain = process.env.PUBLIC_API_HOST_DOMAIN ?? 'localhost';
+
+export const wsApiHostUrl = `${wsApiProtocol}://${apiDomain}:${apiPort}`;
+export const httpApiHostUrl = `${httpApiProtocol}://${apiDomain}:${apiPort}`;
+export const hostUrl = `${hostProtocol}://${hostDomain}:${hostPort}`;
 export const apiHostUrlPrefix = '/api/trpc';
 
 const getNetworkIdCacheKeyFragment = (networkId?: number) => {
