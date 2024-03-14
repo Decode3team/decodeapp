c700b37177dae05786d1729627b358a2852abe84
Drew Palaganas <palaganas.ja@gmail.com>
2024-03-14 11:10:56 +0000

    Updated constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,18 +5,18 @@ dotenv.config();
 
 export const apiPort = process.env.NEXT_PUBLIC_API_HOST_PORT
   ? Number(process.env.NEXT_PUBLIC_API_HOST_PORT)
-  : 3001;
+  : 80;
 
 export const hostPort = process.env.NEXT_PUBLIC_HOST_PORT
   ? Number(process.env.NEXT_PUBLIC_HOST_PORT)
-  : 3000;
+  : 80;
 
-export const httpApiProtocol = process.env.NEXT_PUBLIC_API_HOST_PORT_PROTOCOL ?? 'http';
-export const wsApiProtocol = httpApiProtocol === 'http' ? 'ws' : 'wss';
-export const hostProtocol = process.env.NEXT_PUBLIC_HOST_PORT_PROTOCOL ?? 'http';
+export const httpApiProtocol = process.env.NEXT_PUBLIC_API_HOST_PORT_PROTOCOL ?? 'https';
+export const wsApiProtocol = httpApiProtocol === 'https' ? 'wss' : 'ws';
+export const hostProtocol = process.env.NEXT_PUBLIC_HOST_PORT_PROTOCOL ?? 'https';
 
-export const hostDomain = process.env.NEXT_PUBLIC_HOST_DOMAIN ?? 'localhost';
-export const apiDomain = process.env.NEXT_PUBLIC_API_HOST_DOMAIN ?? 'localhost';
+export const hostDomain = process.env.NEXT_PUBLIC_HOST_DOMAIN ?? 'decode-dev-as.azurewebsites.net';
+export const apiDomain = process.env.NEXT_PUBLIC_API_HOST_DOMAIN ?? 'decode-dev-api-as.azurewebsites.net';
 
 export const wsApiHostUrl = `${wsApiProtocol}://${apiDomain}${apiPort === 80 ? '' : `:${apiPort}`}`;
 export const httpApiHostUrl = `${httpApiProtocol}://${apiDomain}${apiPort === 80 ? '' : `:${apiPort}`}`;
