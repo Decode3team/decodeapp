88e6076540c4ca0e73401bb2d487c4ef0bce6472
palaganas.ja@gmail.com <palaganas.ja@gmail.com>
2024-04-06 16:51:17 +0800

    Added query fields to provider

---
+++ lib/providers/defined-default-fields-provider.ts
@@ -0,0 +1,443 @@
+export class DefinedDefaultFieldsProvider {
+  public static getFilterTokenFields() {
+    return `{
+      priceUSD,
+      liquidity,
+      marketCap,
+      change1,
+      change4,
+      change12,
+      change24,
+      volume1,
+      volume4,
+      volume12,
+      volume24,
+      uniqueTransactions1,
+      uniqueTransactions4,
+      uniqueTransactions12,
+      uniqueTransactions24,
+      token {
+        name,
+        id,
+        address,
+        symbol,
+        networkId,
+        info {
+          imageLargeUrl,
+          imageSmallUrl,
+          imageThumbUrl,
+        }
+      }
+    }`;
+  }
+  public static getDetailedPairStatsFields() {
+    return `{
+      bucketCount,
+      lastTransaction,
+      networkId,
+      pair {
+        address,
+        createdAt,
+        exchangeHash,
+        fee,
+        id,
+        networkId,
+        tickSpacing,
+        token0,
+        token0Data {
+          address,
+          name ,
+          networkId,
+          socialLinks {
+            bitcointalk,
+            blog,
+            coingecko,
+            coinmarketcap,
+            discord,
+            email,
+            facebook,
+            github,
+            instagram,
+            linkedin,
+            reddit,
+            slack,
+            telegram,
+            twitch,
+            twitter,
+            website,
+            wechat,
+            whitepaper,
+            youtube,
+          },
+          symbol,
+          totalSupply,
+          info {
+            circulatingSupply,
+            isScam,
+            imageLargeUrl,
+            imageSmallUrl,
+            imageThumbUrl 
+          }
+        },
+        token1,
+        token1Data {
+          address,
+          name ,
+          networkId,
+          socialLinks {
+            bitcointalk,
+            blog,
+            coingecko,
+            coinmarketcap,
+            discord,
+            email,
+            facebook,
+            github,
+            instagram,
+            linkedin,
+            reddit,
+            slack,
+            telegram,
+            twitch,
+            twitter,
+            website,
+            wechat,
+            whitepaper,
+            youtube,
+          },
+          symbol,
+          totalSupply,
+          info {
+            circulatingSupply,
+            isScam,
+            imageLargeUrl,
+            imageSmallUrl,
+            imageThumbUrl 
+          }
+        }
+      }
+      pairAddress,
+      queryTimestamp
+      stats_day1 {
+        statsNonCurrency {
+          buyers {
+            change,
+            currentValue,
+            previousValue
+          },
+          buys {
+            change,
+            currentValue,
+            previousValue
+          },
+          sellers {
+            change,
+            currentValue,
+            previousValue
+          },
+          sells {
+            change,
+            currentValue,
+            previousValue
+          },
+          traders {
+            change,
+            currentValue,
+            previousValue
+          },
+          transactions {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        statsUsd {
+          buyVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          close {
+            change,
+            currentValue,
+            previousValue
+          }
+          highest {
+            change,
+            currentValue,
+            previousValue
+          }
+          liquidity {
+            change,
+            currentValue,
+            previousValue
+          }
+          lowest {
+            change,
+            currentValue,
+            previousValue
+          }
+          open {
+            change,
+            currentValue,
+            previousValue
+          }
+          sellVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          volume {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        timestamps {
+          start
+          end
+        }
+      },
+      stats_hour1 {
+        statsNonCurrency {
+          buyers {
+            change,
+            currentValue,
+            previousValue
+          },
+          buys {
+            change,
+            currentValue,
+            previousValue
+          },
+          sellers {
+            change,
+            currentValue,
+            previousValue
+          },
+          sells {
+            change,
+            currentValue,
+            previousValue
+          },
+          traders {
+            change,
+            currentValue,
+            previousValue
+          },
+          transactions {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        statsUsd {
+          buyVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          close {
+            change,
+            currentValue,
+            previousValue
+          }
+          highest {
+            change,
+            currentValue,
+            previousValue
+          }
+          liquidity {
+            change,
+            currentValue,
+            previousValue
+          }
+          lowest {
+            change,
+            currentValue,
+            previousValue
+          }
+          open {
+            change,
+            currentValue,
+            previousValue
+          }
+          sellVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          volume {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        timestamps {
+          start
+          end
+        }
+      },
+      stats_hour4 {
+        statsNonCurrency {
+          buyers {
+            change,
+            currentValue,
+            previousValue
+          },
+          buys {
+            change,
+            currentValue,
+            previousValue
+          },
+          sellers {
+            change,
+            currentValue,
+            previousValue
+          },
+          sells {
+            change,
+            currentValue,
+            previousValue
+          },
+          traders {
+            change,
+            currentValue,
+            previousValue
+          },
+          transactions {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        statsUsd {
+          buyVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          close {
+            change,
+            currentValue,
+            previousValue
+          }
+          highest {
+            change,
+            currentValue,
+            previousValue
+          }
+          liquidity {
+            change,
+            currentValue,
+            previousValue
+          }
+          lowest {
+            change,
+            currentValue,
+            previousValue
+          }
+          open {
+            change,
+            currentValue,
+            previousValue
+          }
+          sellVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          volume {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        timestamps {
+          start
+          end
+        }
+      },
+      stats_hour12 {
+        statsNonCurrency {
+          buyers {
+            change,
+            currentValue,
+            previousValue
+          },
+          buys {
+            change,
+            currentValue,
+            previousValue
+          },
+          sellers {
+            change,
+            currentValue,
+            previousValue
+          },
+          sells {
+            change,
+            currentValue,
+            previousValue
+          },
+          traders {
+            change,
+            currentValue,
+            previousValue
+          },
+          transactions {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        statsUsd {
+          buyVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          close {
+            change,
+            currentValue,
+            previousValue
+          }
+          highest {
+            change,
+            currentValue,
+            previousValue
+          }
+          liquidity {
+            change,
+            currentValue,
+            previousValue
+          }
+          lowest {
+            change,
+            currentValue,
+            previousValue
+          }
+          open {
+            change,
+            currentValue,
+            previousValue
+          }
+          sellVolume {
+            change,
+            currentValue,
+            previousValue
+          }
+          volume {
+            change,
+            currentValue,
+            previousValue
+          }
+        },
+        timestamps {
+          start
+          end
+        }
+      }
+    }`;
+  }
+}
